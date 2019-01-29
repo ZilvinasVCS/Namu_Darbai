8f9fcbfabb8abb61d71994662126d6fabb966545
dimacellist <dynovyanovsky@gmail.com>
2019-01-29 20:34:06 +0200

    JS11 Homework added

---
+++ JS11/js/script.js
@@ -0,0 +1,16 @@
+function one() {document.calculator.display.value += '1';}
+function two() {document.calculator.display.value += '2';}
+function three() {calculator.display.value += '3'};
+function four() {calculator.display.value += '4'};
+function five() {calculator.display.value += '5'};
+function six() {calculator.display.value += '6'};
+function seven() {calculator.display.value += '7'};
+function eight() {calculator.display.value += '8'};
+function nine() {calculator.display.value += '9'};
+function zero() {calculator.display.value += '0'};
+function dot() {calculator.display.value += '.'};
+function plius() {calculator.display.value += '+'};
+function minus() {calculator.display.value += '-'};
+function divide() {calculator.display.value += '/'};
+function multiply() {calculator.display.value += '*'};
+function skaicuok() {calculator.display.value = eval(calculator.display.value)};
