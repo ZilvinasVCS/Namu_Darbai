92ef1bdf45ef99f94a79e09c38b49116288b8163
dimacellist <dynovyanovsky@gmail.com>
2019-01-30 21:37:09 +0200

    PHP3 Homework added

---
+++ PHP3/index.php
@@ -0,0 +1,15 @@
+<?php
+    $x = 10;
+    $y = 7;
+
+    $result = $x + $y;
+    echo "$x + $y = $result <br>";
+    $result = $x - $y;
+    echo "$x + $y = $result <br>";
+    $result = $x * $y;
+    echo "$x * $y = $result <br>";
+    $result = $x / $y;
+    echo "$x / $y = $result <br>";
+    $result = $x % $y;
+    echo "$x % $y = $result <br>";
+?>
