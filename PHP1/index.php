41d23efdb2f3f480f65db0ca30056a9c91a32a92
dimacellist <dynovyanovsky@gmail.com>
2019-01-30 20:13:02 +0200

    PHP1 HOmework added

---
+++ PHP1/index.php
@@ -0,0 +1,21 @@
+<?php
+    $country = 'Lietuva';
+    $city = 'Vilnius';
+    $population = 574221;
+?>
+<!DOCTYPE html>
+<html>
+    <head></head>
+    <body>
+        <ul>
+            <li><?php echo $country; ?></li>
+            <li><?php echo $city; ?></li>
+            <li><?php echo $population; ?></li>
+        </ul>
+    </body>         
+</html>   
+        
+        
+   "Vilnius - Lietuvos sostinė"     
+        
+    
