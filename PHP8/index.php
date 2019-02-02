b72f8e4cd9ed29d7478f2c0aef49288160931192
dimacellist <dynovyanovsky@gmail.com>
2019-02-02 14:54:43 +0200

    PHP8 Homework added

---
+++ PHP8/index.php
@@ -0,0 +1,36 @@
+<?php
+   $temp = [4,3,9,19,19,9,12,20,24,20,12,14,18,21,20,23,16,16,15,19,19,17,17,15,12,13,13,15,19,21];
+
+   //Get Average Number
+   $average = array_sum($temp) / count($temp);
+   rsort($temp);
+   echo "Vidutine temperatura: $average <br>";    
+
+   $sum = array_unique($temp);
+
+  echo "Penkios šilčiausios temperatūros: ";
+  $highest = array_slice($sum, 0, 5);
+  foreach ($highest as $temperature) {
+    echo "$temperature, ";
+  }
+  echo '<br>';
+
+  echo "Penkios šalčiausios temperatūros: ";
+  $lowest = array_slice($sum, -5, 5);
+  foreach ($lowest as $temperature) {
+    echo "$temperature, ";
+  }
+?>
+
+<!DOCTYPE html>
+<html lang="lt">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP5</title>
+</head>
+
+<body>
+              
+</body>
+</html>
